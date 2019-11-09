96b6575f1a5727759eec39e94454c0afc6a70e93
nurirppan <pangestu.apps@gmail.com>
2019-11-10 00:32:17 +0700

    create xib splash & welcome page

---
+++ Member Id Project/Apps/Screen/Welcome Screen/WelcomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeVC.swift
+//  Member Id Project
+//
+//  Created by Nur Irfan Pangestu on 10/11/19.
+//  Copyright © 2019 member.id.test. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Member Id Project/Apps/Screen/Welcome Screen/WelcomeView.swift
@@ -0,0 +1,9 @@
+//
+//  WelcomeView.swift
+//  Member Id Project
+//
+//  Created by Nur Irfan Pangestu on 10/11/19.
+//  Copyright © 2019 member.id.test. All rights reserved.
+//
+
+import Foundation
